8ca45bd6b9c77cddf05b7f3dc381448e4fb0a233
pugarchdev <pugarchdev@gmail.com>
2025-11-10 02:17:48 +0530

    "auth update"

--- src/lib/api/authApi.js
+++ src/lib/api/authApi.js
@@ -4,7 +4,9 @@ import axios from "axios";
 
 
 // const API_BASE_URL = "http://localhost:8000/api";
-const API_BASE_URL = "https://saaf-ai-backend.onrender.com/api";
+// const API_BASE_URL = "https://saaf-ai-backend.onrender.com/api";
+const API_BASE_URL = "https://saaf-ai-backend.vercel.app/api"
+
 
 
 export const AuthApi = {
